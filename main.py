3e680bd3e5f44b1f0b342b77a97e6c0c5177d80f
Lawson C <lawson.conallin@gmail.com>
2023-09-11 11:51:09 +1000

    create gitignore and fix up main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import json as j
-client_information=open("client_information.json", r)
-j.load(client_information)
+client_information=open("client_information.json", "r")
+client_information=j.load(client_information)
 print(client_information)
